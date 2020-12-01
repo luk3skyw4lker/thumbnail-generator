275be08693a39511eb387013725ae1680a3fb738
luk3skyw4lker <lucashenriqueblemos@gmail.com>
2020-12-01 19:37:50 -0300

    [feat]: Add bg color acceptance via req.query

--- src/pages/api/thumbnail.ts
+++ src/pages/api/thumbnail.ts
@@ -7,12 +7,13 @@ const isDev = !process.env.AWS_REGION;
 export default async function (req: NextApiRequest, res: NextApiResponse) {
 	try {
 		const title = String(req.query.title);
+		const thumbnail_bg = String(req.query.bg || '#121214');
 
 		if (!title) {
 			throw new Error('Missing title');
 		}
 
-		const html = getThumbnailTemplate({ title, thumbnail_bg: '#121214' });
+		const html = getThumbnailTemplate({ title, thumbnail_bg });
 		const file = await getScreenshot(html, isDev);
 
 		res.setHeader('Content-Type', 'image/png');

8f6908a89ce9b68e6742916237d3cb03dfc90860
luk3skyw4lker <lucashenriqueblemos@gmail.com>
2020-12-01 19:38:08 -0300

    [refactor]: Change home page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
-import Link from 'next/link';
 import { getAllPosts } from './api/posts';
-
+import styles from '../styles/index.module.css';
 interface HomeProps {
 	posts: Array<{
 		slug: string;
@@ -10,17 +9,16 @@ interface HomeProps {
 
 export default function Home(props: HomeProps) {
 	return (
-		<div>
-			<h1>Blog do Lucas</h1>
-			<ul>
-				{props.posts.map((post, idx) => (
-					<li key={idx}>
-						<Link href={post.slug}>
-							<a>{post.title}</a>
-						</Link>
-					</li>
-				))}
-			</ul>
+		<div className={styles.main}>
+			<h1>Serverless Thumb Generator</h1>
+			<p>This is a Thumbnail (or og-image) generator, use it wisely</p>
+
+			<p>
+				Check how to use it and docs on:{' '}
+				<a href="https://github.com/luk3skyw4lker/thumbnail-generator">
+					the GitHub repository
+				</a>
+			</p>
 		</div>
 	);
 }

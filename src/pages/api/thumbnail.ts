f631b1d4722a3298af639ad49e1fcfbdd53d11c5
luk3skyw4lker <lucashenriqueblemos@gmail.com>
2020-12-01 19:07:04 -0300

    [chore]: Initial commit

---
+++ src/pages/api/thumbnail.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getScreenshot } from './_lib/chromium';
+import getThumbnailTemplate from './_lib/thumb_template';
+
+const isDev = !process.env.AWS_REGION;
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+	try {
+		const title = String(req.query.title);
+
+		if (!title) {
+			throw new Error('Missing title');
+		}
+
+		const html = getThumbnailTemplate({ title, thumbnail_bg: '#121214' });
+		const file = await getScreenshot(html, isDev);
+
+		res.setHeader('Content-Type', 'image/png');
+		res.setHeader(
+			'Cache-Control',
+			'public, immutable, no-transform, s-maxage=31536000, max-age=31536000'
+		);
+
+		return res.end(file);
+	} catch (error) {
+		console.error(error);
+
+		return res.status(500).send('Internal error');
+	}
+}
